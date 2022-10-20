2c7afe348ab69c959b249f4f927eeb5e67454568
LIMPIX31 <limpix31@gmail.com>
2022-10-20 14:31:08 +0200

    fix: bind hostname

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.enableCors()
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }))
-  await app.listen(3264)
+  await app.listen(3264, '0.0.0.0')
 }
 
 bootstrap()
